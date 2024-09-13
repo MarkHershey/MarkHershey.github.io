aeef2b1e0272dd30ad5316879fbfbb0284d1c7d7
Mark Huang <hhzeos@gmail.com>
2024-09-13 21:15:13 +0800

    Update

--- markhh/src/sections/Footer.js
+++ markhh/src/sections/Footer.js
@@ -3,7 +3,10 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="mt-5 footer user-select-none">
-            <a href='https://clustrmaps.com/site/1c19b' title='Visit tracker'> </a>
+            <div className="container d-flex flex-row justify-content-center d-none">
+                <a href="https://clustrmaps.com/site/1c19b"><img src="//clustrmaps.com/map_v2.png?cl=080808&w=300&t=n&d=mimMKRAAli-zSS2UDdRbmlLd_oFy2cp_BCUeqsLi8sg&co=ffffff&ct=808080" alt="none" /></a>
+            </div>
+
             <div style={{ height: 70 }}></div>
             <hr className="mt-auto mx-5"></hr>
             <div className="container d-flex flex-row justify-content-center">
